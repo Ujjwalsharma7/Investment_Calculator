8aa0ac0df8458bf601d2ba551f6e99af1d192552
ujjwal <ujjwalsharma1772003@gmail.com>
2023-06-26 16:29:04 +0530

    added formatter

--- src/components/ResultsTable/ResultsTable.js
+++ src/components/ResultsTable/ResultsTable.js
@@ -1,5 +1,12 @@
 import React from 'react'
 
+const formatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
+})
+
 function ResultsTable(props) {
   return (
     <table className="result">
@@ -16,10 +23,10 @@ function ResultsTable(props) {
         {props.data.map((yearData) => (
              <tr key={yearData.year}>
              <td>{yearData.year}</td>
-             <td>{yearData.savingsEndOfYear}</td>
-             <td>{yearData.yearlyInterest}</td>
-             <td>{yearData.savingsEndOfYear - props.initialInvestment - yearData.yearlyContribution*yearData.year}</td>
-             <td>{props.initialInvestment + yearData.yearlyContribution*yearData.year}</td>
+             <td>{formatter.format(yearData.savingsEndOfYear)}</td>
+             <td>{formatter.format(yearData.yearlyInterest)}</td>
+             <td>{formatter.format(yearData.savingsEndOfYear - props.initialInvestment - yearData.yearlyContribution*yearData.year)}</td>
+             <td>{formatter.format(props.initialInvestment + yearData.yearlyContribution*yearData.year)}</td>
            </tr>
         ))}
      
